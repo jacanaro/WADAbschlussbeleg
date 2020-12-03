43c382b70e59529506f97b9843a6a327f970e592
CarolinaTrack <carolinatrack@googlemail.com>
2020-12-03 14:03:55 +0100

    general project update

--- SinglePageApplication/js/logIn.js
+++ SinglePageApplication/js/logIn.js
@@ -1,28 +1,28 @@
 // Loop through Array of Objects
-		var objPeople = [
-			{ // Object @ 0 index
-				username: "admina",
-				password: "mama"
-			},
-			{ // Object @ 1 index
-				username: "normalo",
-				password: "mimi"
-			}
+var objPeople = [
+    { // Object @ 0 index
+        username: "admina",
+        password: "mama"
+    },
+    { // Object @ 1 index
+        username: "normalo",
+        password: "mimi"
+    }
 //ToDo: richtige vorgegebene Passwörter einfügen
-		]
+]
 
-		function getInfo() {
-			var username = document.getElementById('username').value
-			var password = document.getElementById('password').value
+function getInfo() {
+    var username = document.getElementById('username').value
+    var password = document.getElementById('password').value
 
-			for(var i = 0; i < objPeople.length; i++) {
-				// check is user input matches username and password of a current index of the objPeople array
-				if(username == objPeople[i].username && password == objPeople[i].password) {
-					document.getElementById("logIn").style.display='none';
-					document.getElementById("loggedIn").style.display='block';
-					document.getElementById("test").innerHTML=username + " is logged in!!!"; //ToDo: schön formatieren 
-					return
-				}
-			}
-			document.getElementById("test").innerHTML="incorrect username or password"; //ToDo: Popup oder unter Login oder so
-		}
+    for (var i = 0; i < objPeople.length; i++) {
+        // check is user input matches username and password of a current index of the objPeople array
+        if (username == objPeople[i].username && password == objPeople[i].password) {
+            document.getElementById("logIn").style.display = 'none';
+            document.getElementById("loggedIn").style.display = 'block';
+            document.getElementById("test").innerHTML = username + " is logged in!!!"; //ToDo: schön formatieren
+            return
+        }
+    }
+    document.getElementById("test").innerHTML = "incorrect username or password"; //ToDo: Popup oder unter Login oder so
+}
