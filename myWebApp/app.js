59aa1395287049915e9f74abb11d29177e37603f
CarolinaTrack <carolinatrack@googlemail.com>
2021-01-10 19:39:52 +0100

    fix route for contacts; change to /adviz/contacts

--- myWebApp/app.js
+++ myWebApp/app.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/adviz', advizRouter);
 app.use('/index', indexRouter);
 app.use('/users', usersRouter);
-app.use('/contacts', contactsRouter);
+app.use('/adviz/contacts', contactsRouter);
 
 /*
 // catch 404 and forward to error handler
