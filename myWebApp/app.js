7ba4e1dcc3c1de62edf0c4f13decb6572d3dcb47
CarolinaTrack <carolinatrack@googlemail.com>
2021-01-10 16:22:39 +0100

    ticket 10.1: add route /adviz for login

--- myWebApp/app.js
+++ myWebApp/app.js
@@ -1,17 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const contactsRouter = require('./routes/contacts');
+const advizRouter = require('./routes/adviz');
+
+const app = express();
 
 // view engine setup - DAS BRAUCHEN WIR NICHT
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+/*
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+ */
+/*
+app.set('view engine', 'ejs');
+
+ */
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,9 +30,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/adviz', advizRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contacts', contactsRouter);
 
+/*
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -37,5 +51,29 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+ */
+/*
+var ErrorHandler = require('express-error-handler');
+// define below all other routes
+var errorHandler = ErrorHandler({
+  static: {
+    '404': 'error.html', // put this file in your Public folder
+    '500': 'error.html' // ditto
+  }
+  });
+
+// any unresolved requests will 404
+app.use(function(req,res,next) {
+  var err = new Error('Not Found');
+  err.status(404);
+  next(err);
+});
+
+
+app.use(errorHandler);
+
+
+ */
 
 module.exports = app;
