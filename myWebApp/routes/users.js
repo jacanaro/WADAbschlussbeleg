11f97ef497708c2459bf48eb09a2d8f9b1f527a4
CarolinaTrack <carolinatrack@googlemail.com>
2021-01-14 21:07:03 +0100

    make login username and password required again (wasn't working due to button type button instead of submit!)

--- myWebApp/routes/users.js
+++ myWebApp/routes/users.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.status(200).json({ users: "bob"});
-  //res.send('respond with a resource');
 });
 
 module.exports = router;
