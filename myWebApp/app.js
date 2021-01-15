2754ddea33abb11ebe763f71025ea881f84e8902
CarolinaTrack <carolinatrack@googlemail.com>
2021-01-15 19:59:22 +0100

    db update NOT FINISHED AT ALL

--- myWebApp/app.js
+++ myWebApp/app.js
@@ -5,24 +5,11 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const contactsRouter = require('./routes/contacts');
 const advizRouter = require('./routes/adviz');
 
 const app = express();
 
-
-// view engine setup - DAS BRAUCHEN WIR NICHT
-/*
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
- */
-/*
-app.set('view engine', 'ejs');
-
- */
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,47 +18,37 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/adviz', advizRouter);
-app.use('/index', indexRouter);
-app.use('/users', usersRouter);
 app.use('/adviz/contacts', contactsRouter);
 
 /*
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
  */
 /*
-var ErrorHandler = require('express-error-handler');
-// define below all other routes
-var errorHandler = ErrorHandler({
-  static: {
-    '404': 'error.html', // put this file in your Public folder
-    '500': 'error.html' // ditto
-  }
-  });
+app.get("/users", function(req, res) {
 
-// any unresolved requests will 404
-app.use(function(req,res,next) {
-  var err = new Error('Not Found');
-  err.status(404);
-  next(err);
+    MongoClient.connect(url, {useUnifiedTopology: true},
+        function (err, client) {
+            if(err) { //better error handling needed
+                throw err;
+            }
+
+            let db = client.db("advizDB");
+            db.collection("users").find({}).toArray(
+                function(err, result) {
+                    if (err) { //better error handling needed
+                        throw err;
+                    }
+                    console.log(result);
+                    res.status(200).send(result);
+                    client.close();
+                });
+        });
 });
 
-
-app.use(errorHandler);
 
 
  */
