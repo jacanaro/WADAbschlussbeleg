5367fc95f33a796ad34aa26949686f9a0a142c13
jacanaro <aaron_jacob4692@yahoo.de>
2021-01-16 16:21:41 +0100

    implement addNewContact.js server + client

--- myWebApp/public/javascripts/addNewContact.js
+++ myWebApp/public/javascripts/addNewContact.js
@@ -1,6 +1,9 @@
-function addNewContact(){ //UserID wird noch nicht belegt
-    function getAddNewContactFormData(){
-        var newContactObject={"userID": "",
+document.getElementById('addNewContactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    function getAddNewContactFormData() {
+        var newContactObject = {
+            "userID": "",
             "Titel": "",
             "Geschlecht": "",
             "Vorname": "",
@@ -11,22 +14,33 @@ function addNewContact(){ //UserID wird noch nicht belegt
             "Land": "",
             "MailAdr": "",
             "Sonstiges": "",
-            "PrivaterKontakt": false};
-        newContactObject.Titel=document.getElementById("addNewTitle").value;
-        newContactObject.Geschlecht=document.getElementById("addNewGeschlecht").value;
-        newContactObject.Vorname=document.getElementById("addNewVorname").value;
-        newContactObject.Nachname=document.getElementById("addNewNachname").value;
-        newContactObject.StrasseUndHausnummer=document.getElementById("addNewStrasseUndHausnummer").value;
-        newContactObject.PLZ=document.getElementById("addNewPlz").value;
-        newContactObject.Stadt=document.getElementById("addNewStadt").value;
-        newContactObject.Land=document.getElementById("addNewLand").value;
-        newContactObject.MailAdr=document.getElementById("addNewMailAdr").value;
-        newContactObject.Sonstiges=document.getElementById("addNewSonstiges").value;
-        newContactObject.PrivaterKontakt=document.getElementById("addNewPrivatCheck").value;
+            "PrivaterKontakt": false
+        };
+        newContactObject.Titel = document.getElementById("addNewTitle").value;
+        newContactObject.Geschlecht = document.getElementById("addNewGeschlecht").value;
+        newContactObject.Vorname = document.getElementById("addNewVorname").value;
+        newContactObject.Nachname = document.getElementById("addNewNachname").value;
+        newContactObject.StrasseUndHausnummer = document.getElementById("addNewStrasseUndHausnummer").value;
+        newContactObject.PLZ = document.getElementById("addNewPlz").value;
+        newContactObject.Stadt = document.getElementById("addNewStadt").value;
+        newContactObject.Land = document.getElementById("addNewLand").value;
+        newContactObject.MailAdr = document.getElementById("addNewMailAdr").value;
+        newContactObject.Sonstiges = document.getElementById("addNewSonstiges").value;
+        newContactObject.PrivaterKontakt = document.getElementById("addNewPrivatCheck").value;
 
         return newContactObject
 
     }
-    var newContactObject=getAddNewContactFormData();
+
+    var newContactObject = getAddNewContactFormData();
+
+    //send to Client
     createForm(newContactObject);
-}
+    $.post("http://localhost:3000/adviz/contacts",newContactObject, function (data) {
+        if (data === 'yes') {
+            alert("success");
+        } else {
+            alert("login data could not be sent");
+        }
+    });
+});
