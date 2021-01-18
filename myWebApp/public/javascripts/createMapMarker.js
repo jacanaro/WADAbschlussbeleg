5959b0d97f58e7363f949f2eb8cf2aae4c689142
CarolinaTrack <carolinatrack@googlemail.com>
2021-01-18 20:05:46 +0100

    update/fix addNewContact

--- myWebApp/public/javascripts/createMapMarker.js
+++ myWebApp/public/javascripts/createMapMarker.js
@@ -1,15 +1,15 @@
 function createMapMarker(contactObject) {
     var markerReq = new XMLHttpRequest();
     var url = "https://maps.googleapis.com/maps/api/geocode/json?";
-    url = url + "address="+contactObject.StrHsnr+", "+contactObject.Stadt;
+    url = url + "address = " + contactObject.StrHsnr + ", " + contactObject.Stadt;
     url = url + "&key=AIzaSyB6r6VNSQh_pXayQ1yY3-NOp_0rKzaukZ4";
 
     markerReq.open("GET", url, true);
 
-    markerReq.onerror = function () {// Aufruf, wenneinFehlerauftritt
+    markerReq.onerror = function () {   // Aufruf, wenn ein Fehler auftritt
         alert("Connecting to server with " + url + " failed!\n");
     };
-    markerReq.onload = function (e) {// Aufruf,wenndie Anfrage erfolgreich war
+    markerReq.onload = function (e) {   // Aufruf,wenn die Anfrage erfolgreich war
         var data = this.response;
         var obj = JSON.parse(data);
         console.log(obj);
